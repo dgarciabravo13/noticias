26d128edc28425eccff2fcbb6dd37f30c03192f7
David Garcia <dgarciabravo13@gmail.com>
2020-08-19 18:14:23 +0200

    add Form and css module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <>
     <Header titulo="Buscador de Noticias"/>
     <div className="container white">
-      
+      <Formulario/>
     </div>
     </>
   );
