dbf9bca50952fdb997e03b81093d2b8dbae0a6d5
David Garcia <dgarciabravo13@gmail.com>
2020-08-20 11:12:32 +0200

    put the categories in the state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
+
+  //definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState("");
+
   return (
     <>
     <Header titulo="Buscador de Noticias"/>
     <div className="container white">
-      <Formulario/>
+      <Formulario
+        guardarCategoria={guardarCategoria}
+      />
     </div>
     </>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "./Formulario.module.css"
 import useSelect from "../hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
 
   const OPCIONES = [
     {value:"general",label:"General"},
@@ -17,10 +17,20 @@ const Formulario = () => {
 
   const [categoria, SelectNoticias] = useSelect('science', OPCIONES);
 
+  //submit al form, pasar categoria al app.js
+
+  const buscarNoticias = e =>{
+    e.preventDefault();
+
+    guardarCategoria(categoria);
+  }
+
   return ( 
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
-        <form>
+        <form
+          onSubmit={buscarNoticias}
+        >
           <h2 className={styles.heading}>Encuentra noticias por categoría</h2>
           <SelectNoticias/>
           <div className="input-field col s12">
